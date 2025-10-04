fd7650eb6977649ec5a1f4c8f6c482829148c79e
professerXisonCRACK <126928873+professerXisonCRACK@users.noreply.github.com>
2025-10-04 09:53:31 -0500

    Create webserver.py

---
+++ webserver.py
@@ -0,0 +1,37 @@
+import os
+from flask import Flask, request, abort, render_template_string
+import json
+
+app = Flask(__name__)
+
+DATA_FILE = "cousins.json"
+if os.path.exists(DATA_FILE):
+    with open(DATA_FILE) as f:
+        cousins = json.load(f)
+else:
+    cousins = {}
+
+@app.route("/cousin/<user_id>")
+def cousin_profile(user_id):
+    token = request.args.get("token")
+    data = cousins.get(user_id)
+    if not data or data.get("token") != token:
+        return abort(403)
+
+    html = f"""
+    <html>
+    <head><title>{data['name']}'s Profile</title></head>
+    <body>
+        <h1>{data['name']}'s Cousin Profile</h1>
+        <p>Reputation: {data['rep']}/100</p>
+        <p>Rank: #{data['rank']}</p>
+        <p>Cousin ID: {data['cousin_id']}</p>
+        {'<img src="'+data['banner']+'">' if data.get('banner') else ''}
+    </body>
+    </html>
+    """
+    return html
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
